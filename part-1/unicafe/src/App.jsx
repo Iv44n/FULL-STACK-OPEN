30abd9a72f77153cdd64ede073b678b04a58f54f
Iv44n <ivandev2oo6@gmail.com>
2024-07-10 13:46:41 -0500

    part-1 unicafe, step 1

---
+++ part-1/unicafe/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
