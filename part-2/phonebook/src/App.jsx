b1e93d80a2498bee148af1d749336927f6252d75
Iv44n <ivandev2oo6@gmail.com>
2024-07-15 17:24:15 -0500

    part-2 phonebook, step 1

---
+++ part-2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons([{ name: newName }, ...persons]);
+    setNewName("");
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
