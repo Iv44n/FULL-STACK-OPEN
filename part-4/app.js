51204df0a6b8428ffd645a8b1b2b78803d89de50
Iv44n <ivandev2oo6@gmail.com>
2024-07-29 15:10:12 -0500

    part-4 blogList step 2

--- part-4/app.js
+++ part-4/app.js
@@ -1,20 +1,30 @@
 const { MONGODB_URI } = require('./utils/config')
 const express = require('express')
 const app = express()
+const {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+} = require('./utils/middlewares')
 const mongoose = require('mongoose')
 const notesRouter = require('./controllers/blogs')
 const cors = require('cors')
+const logger = require('./utils/logger')
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.error('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 app.use('/api/blogs', notesRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 module.exports = app
---
+++ part-4/utils/middlewares.js
@@ -0,0 +1,31 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
